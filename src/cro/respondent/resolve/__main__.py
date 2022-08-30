a0d49d034e1251fa65875b1d08b92712dd528710
Krystof Pesek <krystof.pesek@gmail.com>
2022-08-30 11:20:49 +0200

    respondent class

--- src/cro/respondent/resolve/__main__.py
+++ src/cro/respondent/resolve/__main__.py
@@ -1,4 +1,31 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import argparse
+from flask import Flask
+from flask_restful import Resource,Api
+
+from cro.respondent.resolve import respondent
+
+import pandas as pd
+
+class HelloWorld(Resource):
+        def get(self):
+            return {'hello': 'world'}
+
+
+
+def main():
+    """
+    The main application function.
+    """
+
+    app = Flask(__name__)
+    api = Api(app)
+
+    api.add_resource(HelloWorld,'/')
+    
+    app.run(debug=True)
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/cro/respondent/resolve/respondent.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from dataclasses import dataclass
+from typing import Iterable, List, Optional, Tuple
+from uuid import UUID
+
+__all__ = tuple(["Respondent"])
+
+
+@dataclass(frozen=True, order=True, slots=True)
+class Respondent:
+    """
+    A class describing OpenMedia respodent
+    """
+
+
+    openmedia_id: str
+    given_name: str
+    family_name: Optional[str]
+    affiliation: str
+    gender: int
+    foreigner: int
+    labels: List[str]
+    matching_ids: List[str]
