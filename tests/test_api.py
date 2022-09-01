3c6c035b1ecde54b54b9e81688dc7fd44dd32c12
Krystof Pesek <krystof.pesek@gmail.com>
2022-09-01 12:52:13 +0200

    sync with upstream

---
+++ tests/test_api.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+import os
+from cro.respondent.resolve.server import create_app
+
+
+def test_that_server_starts():
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/' page is requested (GET)
+    THEN check that the response is valid
+    """
+
+    client = create_app()
+
+    with client.test_client() as test_client:
+        response = test_client.get("/")
+        assert response.status_code == 200
